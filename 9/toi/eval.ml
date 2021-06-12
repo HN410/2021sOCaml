bc9ca522a6636e7e020b756df777cfe8e47c9408
Hn410 <hn410@>
2021-06-12 15:03:44 +0900

    9 example complete

--- 9/toi/eval.ml
+++ 9/toi/eval.ml
@@ -101,4 +101,43 @@ let rec eval_command env c =
     let env' = extend name1 (VRecFun(name1, name2, e, env)) env in
       (name1, env', (VRecFun(name1, name2, e, env)))
 
+
+let rec find_match (p: pattern) (v: value) = 
+  (* パターンと値を照合 
+  　　あっていたら環境のSome型，間違ってたらNoneを返す *)
+  match p with 
+    |PInt a -> 
+      (match v with 
+        | VInt b -> if a = b then Some []
+                    else None 
+        | x -> None )
+    |PBool a -> 
+      (match v with 
+        | VBool  b -> if a = b then Some []
+                    else None
+        | x -> None 
+      )
+    |PVar a -> 
+      Some [(a, v)]
+    |PPair (a0, a1) -> 
+      (match v with 
+        |VPair (b0, b1) -> find_match_2var a0 a1 b0 b1 
+        |x -> None )
+    |PNil -> 
+      (match v with 
+        |VNil -> Some []
+        |x -> None )
+    |PCons (a0, a1) -> 
+        (match v with 
+            |VCons (b0, b1) -> find_match_2var a0 a1 b0 b1 
+            |x -> None )
+    and find_match_2var a0 a1 b0 b1 = 
+      let ans0 = find_match a0 b0 in 
+          let ans1 = find_match a1 b1 in 
+          (match (ans0, ans1) with 
+            | (None, x) -> None
+            | (x, None) -> None 
+            | (Some x0, Some x1) -> 
+              Some (x0@x1))
+              
     
