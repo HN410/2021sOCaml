bffd72bc4739d46f59b700369eaeb15c2ee99010
Hn410 <hn410@>
2021-07-04 22:28:58 +0900

    reversi first

---
+++ reversi/game.ml
@@ -0,0 +1,15 @@
+open Int64
+
+type board = Board of int64 * int64;;
+(* 順に黒，白 *)
+
+let boardSize = 64
+let firstBoard = Board(0x1008000000L, 0x810000000L)
+
+let printBoardIn board now = 
+  let a = 
+
+let printBoard (board: board) = 
+  (* boardをプリントする *)
+  
+
