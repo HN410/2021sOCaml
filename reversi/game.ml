c04d3cd2a0162291091b14544c32485db14dabdc
Hn410 <hn410@>
2021-07-05 00:16:21 +0900

    complete get_legal_move

--- reversi/game.ml
+++ reversi/game.ml
@@ -94,10 +94,46 @@ let get_legal_move_down other_board my_board =
      (legal_shift_n -1) corumn_n in 
       Int64.logand (Int64.shift_right_logical new_mine corumn_n) blank 
 
+let get_legal_move_du other_board my_board plus = 
+(* 斜め下方向だけの合法マス用 my_board側の手番の人がおける場所*)
+  let blank = Int64.lognot (Int64.logor my_board other_board) in 
+  let mine_mask = Int64.logand my_board diagonal_legal_mask in 
+  let mine = Int64.logand (Int64.shift_right_logical other_board (corumn_n+plus))
+   mine_mask in 
+   let new_mine = get_legal_move_right_in mine mine_mask 
+     (legal_shift_n -1) (corumn_n+plus) in 
+      Int64.logand (Int64.shift_right_logical new_mine (corumn_n+plus)) blank 
 
+let get_legal_move_dd other_board my_board plus = 
+(* 斜め上方向だけの合法マス用 my_board側の手番の人がおける場所*)
+  let blank = Int64.lognot (Int64.logor my_board other_board) in 
+  let mine_mask = Int64.logand my_board diagonal_legal_mask in 
+  let mine = Int64.logand (Int64.shift_left other_board (corumn_n+plus))
+   mine_mask in 
+   let new_mine = get_legal_move_left_in mine mine_mask 
+     (legal_shift_n -1) (corumn_n+plus) in 
+      Int64.logand (Int64.shift_left new_mine (corumn_n+plus)) blank 
 
-let get_legal_move (board: board) turn = 1
+let get_legal_move_d other_board my_board = 
+  let lu = get_legal_move_du other_board my_board 1 in 
+  let ru = get_legal_move_du other_board my_board (-1) in 
+  let ld = get_legal_move_dd other_board my_board (-1) in
+  let rd = get_legal_move_dd other_board my_board 1 in 
+  Int64.logor (Int64.logor lu ru) (Int64.logor ld rd)
+   
+  
+
+let get_legal_move (board: board) turn = 
   (* turnにとって合法なマス一覧となるint64を返す *)
+  let Board(other_board, my_board) = 
+    (if(turn = 1) then board else 
+    let Board(a, b) = board in Board(b, a)) in 
+  let left = get_legal_move_left other_board my_board in 
+  let right = get_legal_move_right other_board my_board in 
+  let up = get_legal_move_up other_board my_board in 
+  let down = get_legal_move_down other_board my_board in 
+  let d = get_legal_move_d other_board my_board in 
+  Int64.logor (Int64.logor left right) (Int64.logor (Int64.logor up down) d)
 
 let test_white = 0x10787820301000L
 let test_black = 0x2002045c0c0000L
