bb28f66001ee28c5ff1a8b66f862350c26150005
Hn410 <hn410@>
2021-07-26 23:05:27 +0900

    add game.mli, makefile

---
+++ reversi/game.mli
@@ -0,0 +1,13 @@
+type board
+
+val first_board : board 
+val board_size  : int
+val corumn_n    : int 
+val black_mark  : string
+val white_mark  : string
+val none_mark   : string 
+val black_turn  : int 
+val white_turn  : int 
+
+
+val print_board : board -> unit
