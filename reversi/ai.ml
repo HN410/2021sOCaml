848af0033168d5928385d677d93d562326450eb7
Hn410 <hn410@>
2021-07-27 00:23:24 +0900

    add v0 judge

--- reversi/ai.ml
+++ reversi/ai.ml
@@ -6,6 +6,10 @@ let pc_mask1 = 0x3333333333333333L
 let pc_mask2 = 0x0f0f0f0f0f0f0f0fL
 let pc_mask3 = 0x7fL
 
+let final_border = 50
+
+let eval_v0_factor = -5
+
 
 let piece_count board = 
   let a0 = Int64.sub board (Int64.logand 
@@ -20,6 +24,16 @@ let piece_count board =
   let a5 = Int64.add a4 (Int64.shift_right_logical a4 32) in 
   Int64.to_int (Int64.logand a5 pc_mask3)
 
-let get_evaluation_value (board: board) (turn: int ) = 
+let get_evaluation_value (board: board) (turn: int ) (moves: int) = 
   (* 評価値 *)
-  
+  let (myboard, otherboard) = 
+    if turn = black_turn then get_board_tuple board 
+    else let (a, b) = get_board_tuple board in (b, a) in
+  let v0 = if turn > final_border then 0 
+    (* 自分の着手可能数- 相手の着手可能数 *)
+           else (piece_count (get_legal_move board turn)) - 
+                (piece_count (get_legal_move board (get_another_turn turn ))) in
+  let ans = v0 * eval_v0_factor in ans
+
+
+
