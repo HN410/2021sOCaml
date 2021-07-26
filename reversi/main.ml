cab3ddad75d8cfd54766ace3bbe5246614bde31d
Hn410 <hn410@>
2021-07-27 00:47:08 +0900

    main for evaluation value

--- reversi/main.ml
+++ reversi/main.ml
@@ -5,9 +5,14 @@ open Ai
 let test_white = 0x10787820301000L
 let test_black = 0x2002045c0c0000L
 let test_white_1 = 0x007e7e7e2c7e7e00L
-let test_black_1 = 0xff818181428181ffL
+let test_black_1 = 0xff818181428181ffL;;
+let test_board = make_board test_black test_white;;
 
 let new_black = 0x8000000000L
 let new_black_1 = 0x10000000L ;;
 
-(print_board (make_board test_black test_white))
+(print_board (test_board));
+print_newline();
+print_int (piece_count test_white);
+print_newline();
+print_int (get_evaluation_value test_board black_turn 20);
