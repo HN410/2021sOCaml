c4cb01269f50363c8725c5bd5a83352df5ff1142
Hn410 <hn410@>
2021-07-26 23:11:08 +0900

    comp game.mli

--- reversi/game.mli
+++ reversi/game.mli
@@ -10,4 +10,12 @@ val black_turn  : int
 val white_turn  : int 
 
 
-val print_board : board -> unit
+val print_board         : board -> unit
+(* boardを表示 *)
+val print_only_board    : int64 -> int -> unit 
+(* turnの手番の駒だけ表示 *)
+val get_legal_move      : board -> int -> int64
+(* turnにとって合法なマス一覧となるint64を返す *)
+val flip                : board -> int64 -> int -> board 
+  (*boardにnewoneを置いたときの盤面を返す*)
+  
