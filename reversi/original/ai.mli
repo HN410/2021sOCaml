dab29978672584499cdd76eaa819b4aee2135b74
Hn410 <hn410@>
2021-07-27 13:15:20 +0900

    change board board64

--- reversi/original/ai.mli
+++ reversi/original/ai.mli
@@ -3,7 +3,7 @@ open Game
 type aimove
 val piece_count         : int64 -> int
 (*ある石の盤とみなして駒の数を数える *)
-val get_evaluation_value: board -> int -> int ->  int 
+val get_evaluation_value: board64 -> int -> int ->  int 
  (*盤面を受け取り，その盤面でのある手番から見た
  　評価値を返す
    入力: board, turn, moves*)
